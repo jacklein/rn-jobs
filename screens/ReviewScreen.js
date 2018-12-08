6ab9ae0edcf713f838d84e63df85a98c12ddba21
Jack K <j.klein159@yahoo.com>
2018-12-08 15:21:10 -0500

    added styles to ReviewScreen header

--- screens/ReviewScreen.js
+++ screens/ReviewScreen.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, Platform } from 'react-native';
 import { Button } from 'react-native-elements'
 
 class ReviewScreen extends Component {
@@ -7,8 +7,16 @@ class ReviewScreen extends Component {
     return {
       headerTitle: 'Review Jobs',
       headerRight: (
-        <Button title="Settings" onPress={() => navigation.navigate('settings')}/>
-      )
+        <Button 
+          title="Settings" 
+          onPress={() => navigation.navigate('settings')}
+          backgroundColor="rgba(0,0,0,0)"
+          color="rgba(0, 122, 255, 1)"
+        />
+      ),
+      headerStyle: {
+        marginTop: Platform.OS === 'android' ? 24 : 0
+      }
     }
   }
 
