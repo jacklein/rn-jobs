72d1f2e2ac10c4e018d7ce8ae2ca025a4e864722
Jack K <j.klein159@yahoo.com>
2018-12-08 15:09:44 -0500

    setup basic app navigation

--- App.js
+++ App.js
@@ -1,15 +1,36 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { createBottomTabNavigator, createStackNavigator, createAppContainer } from 'react-navigation';
 
-export default class App extends React.Component {
+import AuthScreen from './screens/AuthScreen';
+import WelcomeScreen from './screens/WelcomeScreen';
+import MapScreen from './screens/MapScreen';
+import DeckScreen from './screens/DeckScreen';
+import ReviewScreen from './screens/ReviewScreen';
+import SettingsScreen from './screens/SettingsScreen';
+
+class App extends React.Component {
   render() {
     return (
       <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
+        <AppNavigator />
       </View>
     );
   }
 }
+
+const AppNavigator = createBottomTabNavigator({
+  welcome: WelcomeScreen,
+  auth: AuthScreen,
+  main: createBottomTabNavigator({
+    map: MapScreen,
+    deck: DeckScreen,
+    review: createStackNavigator({
+      review: ReviewScreen,
+      settings: SettingsScreen
+    })
+  })
+});
 
 const styles = StyleSheet.create({
   container: {
@@ -19,3 +40,5 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
   },
 });
+
+export default createAppContainer(AppNavigator);
---
+++ screens/ReviewScreen.js
@@ -0,0 +1,27 @@
+import React, { Component } from 'react';
+import { View, Text } from 'react-native';
+import { Button } from 'react-native-elements'
+
+class ReviewScreen extends Component {
+  static navigationOptions = ({ navigation }) => {
+    return {
+      headerTitle: 'Review Jobs',
+      headerRight: (
+        <Button title="Settings" onPress={() => navigation.navigate('settings')}/>
+      )
+    }
+  }
+
+  render() {
+    return (
+      <View>
+        <Text>ReviewScreen</Text>
+        <Text>ReviewScreen</Text>
+        <Text>ReviewScreen</Text>
+        <Text>ReviewScreen</Text>
+      </View>
+    );
+  }
+}
+
+export default ReviewScreen;
