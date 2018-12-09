484620f2c343287d31e9a75317db42441e9fcce4
Jack K <j.klein159@yahoo.com>
2018-12-09 15:50:25 -0500

    added job swipe deck to DeckScreen

--- actions/job_actions.js
+++ actions/job_actions.js
@@ -23,13 +23,14 @@ const buildJobsUrl = zip => {
   return `${JOB_ROOT_URL}${query}`;
 }
 
-export const fetchJobs = region => async dispatch => {
+export const fetchJobs = (region, callback) => async dispatch => {
   try{
     //let zip = await reverseGeocode(region);
     //const url = buildJobsUrl(zip);
     //let { data } = await axios.get(url);
     const data = JOB_DATA;
     dispatch({ type: FETCH_JOBS, payload: data });
+    callback();
   } catch(err) {
     console.log(err);
   }
